8867afe6f855eaeb69f32b4832b19a637b9fabf4
ishchenkom <ishchenkom@apomeds.com>
2023-03-30 18:39:48 +0300

    end

--- src/project/website/apomeds/pages/choose_store.py
+++ src/project/website/apomeds/pages/choose_store.py
@@ -1,3 +1,4 @@
+import allure
 from playwright.sync_api import Page
 
 
@@ -5,5 +6,15 @@ class ChooseStore(object):
     def __init__(self, page: Page):
         self.page = page
 
+    @allure.step('click on the de store tile')
     def choose_de_store(self):
-        self.page.click('id=de')
+        try:
+            el = self.page.locator('id=de')
+            screen = self.page.screenshot(path='allure-results/test_screen1.png', full_page=True)
+            allure.attach(screen, attachment_type=allure.attachment_type.PNG)
+        except Exception:
+            print('ERROR')
+            screen = self.page.screenshot(path='allure-results/test_screen.png', full_page=True)
+            allure.attach(screen, attachment_type=allure.attachment_type.PNG)
+            raise
+        el.click()
--- src/project/website/apomeds/pages/home.py
+++ src/project/website/apomeds/pages/home.py
@@ -1,3 +1,4 @@
+import allure
 from playwright.sync_api import Page
 
 
@@ -5,5 +6,6 @@ class HomePage(object):
     def __init__(self, page: Page):
         self.page = page
 
+    @allure.step('click on the hair loss category')
     def choose_hairloss_category(self):
         self.page.click('id=32')
